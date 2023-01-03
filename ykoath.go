aefeeb4a3d6eb9d292b1f79b2d4848d30b465cfa
Les Aker <me@lesaker.org>
2023-01-03 01:32:57 -0500

    allow serial number to be set

--- ykoath.go
+++ ykoath.go
@@ -1,7 +1,9 @@
 package ykoath
 
 import (
+	"encoding/binary"
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 
@@ -37,12 +39,17 @@ const (
 	errFailedToListSuitableReader = "no suitable reader found (out of %d readers)"
 	errFailedToReleaseContext     = "failed to release context"
 	errFailedToTransmit           = "failed to transmit APDU"
+	errFailedToReadSerial         = "failed to read serial"
 	errUnknownTag                 = "unknown tag (%x)"
 )
 
 // New initializes a new OATH session
 func New() (*OATH, error) {
+	return NewFromSerial("")
+}
 
+// NewFromSerial creates an OATH session for a specific key
+func NewFromSerial(serial string) (*OATH, error) {
 	context, err := scard.EstablishContext()
 
 	if err != nil {
@@ -57,26 +64,37 @@ func New() (*OATH, error) {
 
 	for _, reader := range readers {
 
-		if strings.Contains(strings.ToLower(reader), "yubikey") {
-
-			card, err := context.Connect(reader, scard.ShareShared, scard.ProtocolAny)
-
-			if err != nil {
-				return nil, errors.Wrapf(err, errFailedToConnect)
-			}
-
-			return &OATH{
-				card:    card,
-				Clock:   time.Now,
-				context: context,
-			}, nil
-
+		if !strings.Contains(strings.ToLower(reader), "yubikey") {
+			continue
 		}
 
+		card, err := context.Connect(reader, scard.ShareShared, scard.ProtocolAny)
+
+		if err != nil {
+			return nil, errors.Wrapf(err, errFailedToConnect)
+		}
+
+		o := OATH{
+			card:    card,
+			Clock:   time.Now,
+			context: context,
+		}
+
+		if serial == "" {
+			return &o, nil
+		}
+
+		cardSerial, err := o.Serial()
+		if err != nil {
+			return nil, errors.Wrapf(err, errFailedToReadSerial)
+		}
+
+		if serial == cardSerial {
+			return &o, nil
+		}
 	}
 
 	return nil, fmt.Errorf(errFailedToListSuitableReader, len(readers))
-
 }
 
 // Close terminates an OATH session
@@ -92,6 +110,24 @@ func (o *OATH) Close() error {
 
 	return nil
 
+}
+
+func (o *OATH) Serial() (string, error) {
+	_, err := o.card.Transmit([]byte{0x00, 0xA4, 0x04, 0x00, 0x08, 0xA0, 0x00, 0x00, 0x05, 0x27, 0x47, 0x11, 0x17})
+	if err != nil {
+		return "", err
+	}
+	resp, err := o.card.Transmit([]byte{0x00, 0x1d, 0x00, 0x00})
+	if err != nil {
+		return "", err
+	}
+	kvs := read(resp[1 : len(resp)-2])
+	for _, item := range kvs {
+		if item.tag == 0x02 {
+			return strconv.FormatUint(uint64(binary.BigEndian.Uint32(item.value)), 10), nil
+		}
+	}
+	return "", errors.Wrapf(fmt.Errorf("no serial tag found"), errFailedToReleaseContext)
 }
 
 // send sends an APDU to the card
